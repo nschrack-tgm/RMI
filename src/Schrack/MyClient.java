2fd357212bd70fe1d9e08f1e48c555ea362b1442
Nikolaus Schrack <nschrack@student.tgm.ac.at>
2013-12-03 16:53:22 +0100

    Added Client

--- src/Schrack/MyClient.java
+++ src/Schrack/MyClient.java
@@ -4,14 +4,14 @@ package Schrack;
 import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.io.*;
+
 public class MyClient {
 	public static void main( String args[] ) throws Exception {
 		if ( System.getSecurityManager() == null ) {
 			System.setSecurityManager( new RMISecurityManager() ); 
 		}
-		Registry registry = LocateRegistry.createRegistry( Registry.REGISTRY_PORT );
-		Calculator p= ( Calculator ) Naming.lookup( "rmi://my.host.edu/PiServer" );
+		Registry registry = LocateRegistry.getRegistry(1099);
+		Calculator p = ( Calculator ) registry.lookup( "rmi://my.host.edu/PiServer" );
 		
 		System.out.println ( p.pi(17) );
 		}

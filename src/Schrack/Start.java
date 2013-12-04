dc28c8ffb77efd2762698181bb69cd4c28662431
Nikolaus Schrack <nschrack@student.tgm.ac.at>
2013-12-04 11:08:51 +0100

    Server with name

--- src/Schrack/Start.java
+++ src/Schrack/Start.java
@@ -4,9 +4,30 @@ import java.rmi.RemoteException;
 
 public class Start {
 	public static void main (String args[]) throws RemoteException { 
-		PiServer pi= new PiServer("PiServer");
-		pi.register();
 		
+		String [] login =new String [1];
+
 		
+		//Einlesen der Werte
+		for(int i=0; i<args.length; i++){
+		
+				if(args[i].equals("-n"))
+				{
+					login[0]=args[i+1];
+					i++;
+				}else if(args[i].equals("help")){
+					System.err.println("Usage: [-n NameOfServer ]");
+					System.exit(1);
+
+				}else{
+					System.err.println("Usage: [-n NameOfServer ]");
+					System.exit(1);
+				
+				}	
 		}
+		
+		PiServer pi= new PiServer(login[0]);
+		pi.register();
+
+	}
 }
